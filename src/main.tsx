979be6bcecbaa585dfde119981da6d6a86c0e35e
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-12 13:40:35 -0300

    Adicionando ajustes

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,32 @@
-// seu main.tsx
-import React, { lazy } from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './firebase'
 import './index.css'
 
-const Room = lazy(() => import('./Room'))
 const App = lazy(() => import('./App'))
+const Room = lazy(() => import('./Room'))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/room/:roomId" element={<Room />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="min-h-screen flex items-center justify-center bg-gray-50">
+            <div className="flex items-center space-x-2">
+              <div className="w-4 h-4 rounded-full animate-pulse bg-blue-600"></div>
+              <div className="w-4 h-4 rounded-full animate-pulse bg-blue-600"></div>
+              <div className="w-4 h-4 rounded-full animate-pulse bg-blue-600"></div>
+              <span className="text-lg text-gray-700">Carregando...</span>
+            </div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 )
