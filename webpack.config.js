37883b956de5b86f5b60d95d8ec56ace405cfe65
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-12 16:48:30 -0300

    Adicionando ajustes webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,23 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const webpack = require('webpack')
-const dotenv = require('dotenv')
+const TerserPlugin = require('terser-webpack-plugin')
 
 // Carregar variáveis de ambiente do .env
-dotenv.config()
+// dotenv.config(); // Removido, pois a Vercel já injeta
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production'
 
   // Define as variáveis de ambiente que serão injetadas no código do cliente
-  // Garante que 'process.env' existe e injeta as variáveis REACT_APP_ nele.
   const definedEnv = {
-    // Explicitamente define 'process' como um objeto global vazio se ele não existir.
-    // Isso é uma medida de segurança para garantir que 'process' esteja definido.
     process: '{}',
-    // Define 'process.env' como um objeto vazio para começar.
     'process.env': {},
-    // Define NODE_ENV, que é frequentemente usado por bibliotecas.
     'process.env.NODE_ENV': JSON.stringify(
       isProduction ? 'production' : 'development'
     )
   }
 
-  // Adiciona todas as variáveis de ambiente que começam com REACT_APP_
-  // Elas serão injetadas como propriedades de 'process.env'
   for (const key in process.env) {
     if (key.startsWith('REACT_APP_')) {
       definedEnv[`process.env.${key}`] = JSON.stringify(process.env[key])
@@ -61,28 +54,38 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './public/index.html' // Seu arquivo HTML de template
       }),
-      // Use o DefinePlugin para injetar as variáveis de ambiente
       new webpack.DefinePlugin(definedEnv),
-      // Adiciona um plugin para injetar um polyfill para 'process' no início do bundle
       new webpack.ProvidePlugin({
-        process: 'process/browser' // Isso injeta um polyfill para 'process'
+        process: 'process/browser'
       }),
-      // Adicione o BundleAnalyzerPlugin apenas em modo de produção
       isProduction &&
         new BundleAnalyzerPlugin({
-          analyzerMode: 'static', // Gera um arquivo HTML estático
-          reportFilename: 'bundle-report.html', // Nome do arquivo de relatório
-          openAnalyzer: false // Não abre automaticamente no navegador
+          analyzerMode: 'static',
+          reportFilename: 'bundle-report.html',
+          openAnalyzer: false
         })
-    ].filter(Boolean), // Filtra plugins nulos (para quando isProduction é falso)
+    ].filter(Boolean),
     devServer: {
-      historyApiFallback: true, // Necessário para o React Router DOM
+      historyApiFallback: true,
       port: 3000
     },
     optimization: {
+      minimize: false, // TEMPORARIAMENTE DESATIVADO PARA DEBUG: Habilita a minificação em produção
+      // minimizer: [ // Comentado para desativar o Terser
+      //   new TerserPlugin({
+      //     terserOptions: {
+      //       ecma: 2024,
+      //       compress: {},
+      //       output: {
+      //         comments: false,
+      //       },
+      //       sourceMap: false,
+      //     },
+      //     extractComments: false,
+      //   }),
+      // ],,
       splitChunks: {
-        chunks: 'all' // Otimiza a divisão de chunks para todos os tipos de módulos
-        // Você pode configurar minSize, maxSize, cacheGroups aqui para controle mais granular
+        chunks: 'all'
       }
     }
   }
