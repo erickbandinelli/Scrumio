7465196baf360b2ce9328330704e594da51b157d
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-13 00:40:01 -0300

    Adicionando ajustes webpack

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,26 +10,14 @@ import { getDatabase } from 'firebase/database'
 
 declare const __initial_auth_token: string | undefined
 
-declare const process: {
-  env: {
-    FIREBASE_API_KEY: string
-    FIREBASE_AUTH_DOMAIN: string
-    FIREBASE_DATABASE_URL: string
-    FIREBASE_PROJECT_ID: string
-    FIREBASE_STORAGE_BUCKET: string
-    FIREBASE_MESSAGING_SENDER_ID: string
-    FIREBASE_APP_ID: string
-  }
-}
-
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: 'process.env.FIREBASE_API_KEY',
+  authDomain: 'process.env.FIREBASE_AUTH_DOMAIN',
+  databaseURL: 'process.env.FIREBASE_DATABASE_URL',
+  projectId: 'process.env.FIREBASE_PROJECT_ID',
+  storageBucket: 'process.env.FIREBASE_STORAGE_BUCKET',
+  messagingSenderId: 'process.env.FIREBASE_MESSAGING_SENDER_ID',
+  appId: 'process.env.FIREBASE_APP_ID'
 }
 
 // Initialize Firebase
