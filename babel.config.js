9daf1effbca377ba39d08fe38ac143a160c391b9
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-12 23:56:54 -0300

    Adicionando ajustes webpack

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+// babel.config.js
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true
+        }
+      }
+    ],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript'
+  ],
+  plugins: []
+}
