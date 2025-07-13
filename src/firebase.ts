8e803f105a541226112558242a1ca8c18c719069
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-13 00:35:54 -0300

    Adicionando ajustes webpack

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,27 +8,28 @@ import {
 } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
-// Declare global variables provided by the Canvas environment
-declare const __app_id: string | undefined
-declare const __firebase_config: string | undefined
 declare const __initial_auth_token: string | undefined
 
-const configFromCanvas =
-  typeof __firebase_config !== 'undefined' ? JSON.parse(__firebase_config) : {}
+declare const process: {
+  env: {
+    FIREBASE_API_KEY: string
+    FIREBASE_AUTH_DOMAIN: string
+    FIREBASE_DATABASE_URL: string
+    FIREBASE_PROJECT_ID: string
+    FIREBASE_STORAGE_BUCKET: string
+    FIREBASE_MESSAGING_SENDER_ID: string
+    FIREBASE_APP_ID: string
+  }
+}
 
-// Firebase configuration provided by the user
 const firebaseConfig = {
-  apiKey: configFromCanvas.apiKey || 'process.env.FIREBASE_API_KEY',
-  authDomain: configFromCanvas.authDomain || 'process.env.FIREBASE_AUTH_DOMAIN',
-  databaseURL:
-    configFromCanvas.databaseURL || 'process.env.FIREBASE_DATABASE_URL',
-  projectId: configFromCanvas.projectId || 'process.env.FIREBASE_PROJECT_ID',
-  storageBucket:
-    configFromCanvas.storageBucket || 'process.env.FIREBASE_STORAGE_BUCKET',
-  messagingSenderId:
-    configFromCanvas.messagingSenderId ||
-    'process.env.FIREBASE_MESSAGING_SENDER_ID',
-  appId: configFromCanvas.appId || 'process.env.FIREBASE_APP_ID'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 // Initialize Firebase
