eed7a750dba1ec1f1ad7424e664f5bd6d59a50be
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-13 00:06:26 -0300

    Adicionando ajustes webpack

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,13 +15,34 @@ declare const __initial_auth_token: string | undefined
 
 // Firebase configuration provided by the user
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:
+    (typeof __firebase_config !== 'undefined' &&
+      JSON.parse(__firebase_config).apiKey) ||
+    process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 // Initialize Firebase
