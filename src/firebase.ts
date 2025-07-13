8f6a4a6e4814eace98905c7a1a1580f35a25c23f
Erick de Oliveira <erickbandinellioliver@gmail.com>
2025-07-13 00:28:06 -0300

    Adicionando ajustes webpack

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,34 +15,13 @@ declare const __initial_auth_token: string | undefined
 
 // Firebase configuration provided by the user
 const firebaseConfig = {
-  apiKey:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_API_KEY,
-  authDomain:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_DATABASE_URL,
-  projectId:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_PROJECT_ID,
-  storageBucket:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId:
-    (typeof __firebase_config !== 'undefined' &&
-      JSON.parse(__firebase_config).apiKey) ||
-    process.env.FIREBASE_APP_ID
+  apiKey: process.env.FIREBASE_API_KEY || '',
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
+  databaseURL: process.env.FIREBASE_DATABASE_URL || '',
+  projectId: process.env.FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.FIREBASE_APP_ID || ''
 }
 
 // Initialize Firebase
